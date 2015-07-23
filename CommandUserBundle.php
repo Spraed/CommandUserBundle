86aeada2360c6e8ea1567407ebccaf5e2afcd227
Stefan Blanke <stedekay@posteo.de>
2015-07-23 21:02:03 +0200

    add CommandUserBundle class

---
+++ CommandUserBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Spraed\CommandUserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Stefan Blanke <stedekay@posteo.de>
+ */
+class CommandUserBundle extends Bundle
+{
+}
